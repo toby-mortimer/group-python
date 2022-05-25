9ebc7a3e51d52e297a5141e3c465334d89aa02ad
toby-mortimer <tmortimer@s6c.mlp.college>
2022-05-25 11:53:51 +0100

    many things

--- group-python/1.py
+++ group-python/1.py
@@ -1,12 +1,14 @@
 from db import DB
 from PeopleTable import People
 
-database = DB('test.db')
-conn = database.Conn
-people = People(conn)
 
-conn.execute('SELECT * FROM People')
-cursor = conn.cursor()
-data = cursor.fetchall()
+def __init__(self, conn, query):
+    database = DB('test.db')
+    conn = database.Conn
+    people = People(query)
 
-print(data)
+    conn.execute('SELECT * FROM People')
+    cursor = conn.cursor()
+    data = cursor.fetchall()
+
+    print(data)
